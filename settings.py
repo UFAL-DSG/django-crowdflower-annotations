ef04b41ffcaf66ad44569ec50197639d3b02dd96
Matěj Korvas <korvas@ufal.mff.cuni.cz>
2013-09-03 10:56:40 +0200

    Tiny change: SITE_ID changed to 2.

--- settings.py
+++ settings.py
@@ -79,7 +79,7 @@ TIME_ZONE = 'Europe/Prague'
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-gb'
 
-SITE_ID = 1
+SITE_ID = 2
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
